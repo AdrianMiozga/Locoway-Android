7d55557189668505503f1d20ec2898af7c1e1c9a
Adrian Miozga <AdrianMiozga@outlook.com>
2024-05-08 15:41:16 +0200

    Add `signingConfig` for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,8 @@ android {
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
+
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
