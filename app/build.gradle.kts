ce624ec2714bfb2ab50771cfdad0c51592ee5a72
Adrian Miozga <AdrianMiozga@outlook.com>
2024-05-08 13:56:39 +0200

    Add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.core.splashscreen)
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
--- app/src/main/java/com/wentura/pkp_android/MainActivity.kt
+++ app/src/main/java/com/wentura/pkp_android/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.wentura.pkp_android.ui.PKPApp
 import com.wentura.pkp_android.ui.theme.PKPAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         enableEdgeToEdge()
 
