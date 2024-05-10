25de6109650863da5cf6d5d840052e7c7867b048
Adrian Miozga <AdrianMiozga@outlook.com>
2024-05-10 15:55:11 +0200

    Add tabs and pager

--- app/src/main/java/com/wentura/pkp_android/compose/login/Login.kt
+++ app/src/main/java/com/wentura/pkp_android/compose/login/Login.kt
@@ -1,25 +1,75 @@
 package com.wentura.pkp_android.compose.login
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wentura.pkp_android.R
+import com.wentura.pkp_android.ui.PKPAndroidTheme
+import kotlinx.coroutines.launch
 
+enum class LoginPage(@StringRes val titleResId: Int) {
+    LOGIN(R.string.login), REGISTER(R.string.register)
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
-fun Login(onUpClick: () -> Unit = {}) {
+fun Login(onUpClick: () -> Unit = {}, pages: Array<LoginPage> = LoginPage.entries.toTypedArray()) {
+    val pagerState = rememberPagerState(pageCount = { pages.size })
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(topBar = { LoginTopAppBar(onUpClick = onUpClick) }) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-            Text("login")
+            PrimaryTabRow(selectedTabIndex = pagerState.currentPage) {
+                pages.forEachIndexed { index, page ->
+                    Tab(
+                        selected = pagerState.currentPage == index,
+                        onClick = { coroutineScope.launch { pagerState.animateScrollToPage(index) } },
+                        unselectedContentColor = MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.height(49.dp)
+                    ) {
+                        Text(stringResource(page.titleResId))
+                    }
+                }
+            }
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.fillMaxHeight(),
+                verticalAlignment = Alignment.Top
+            ) { index ->
+                when (pages[index]) {
+                    LoginPage.LOGIN -> {
+                        Text("Login")
+                    }
+
+                    LoginPage.REGISTER -> {
+                        Text("Register")
+                    }
+                }
+            }
         }
     }
 }
@@ -34,4 +84,12 @@ private fun LoginTopAppBar(onUpClick: () -> Unit) {
             Icon(imageVector = Icons.AutoMirrored.Outlined.ArrowBack, contentDescription = null)
         }
     })
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun LoginPreview() {
+    PKPAndroidTheme {
+        Login()
+    }
 }
