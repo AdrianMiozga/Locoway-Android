f3fc0cce1dc43dfc11c61b940bf4e289cc58af3d
Adrian Miozga <AdrianMiozga@outlook.com>
2024-05-17 20:07:42 +0200

    Move home navigation drawer to different file

---
+++ app/src/main/java/com/wentura/pkp_android/compose/home/HomeNavigationDrawer.kt
@@ -0,0 +1,152 @@
+package com.wentura.pkp_android.compose.home
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.wentura.pkp_android.R
+import com.wentura.pkp_android.ui.PKPAndroidTheme
+import com.wentura.pkp_android.viewmodels.HomeUiState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeNavigationDrawer(
+    uiState: StateFlow<HomeUiState>,
+    drawerState: DrawerState,
+    scope: CoroutineScope = rememberCoroutineScope(),
+    onLoginClick: () -> Unit = {},
+    onMyAccountClick: () -> Unit = {},
+    onMyTicketsClick: () -> Unit = {},
+    onPassengersClick: () -> Unit = {},
+    content: @Composable () -> Unit = {},
+) {
+    ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
+        ModalDrawerSheet(modifier = Modifier.width(252.dp)) {
+            Text(
+                stringResource(R.string.app_name),
+                modifier = Modifier.padding(start = 28.dp, end = 28.dp, top = 16.dp)
+            )
+
+            HorizontalDivider(modifier = Modifier.padding(horizontal = 28.dp, vertical = 16.dp))
+
+            NavigationDrawerItem(
+                label = { Text(text = stringResource(R.string.connection_search)) },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Search, contentDescription = null
+                    )
+                },
+                selected = true,
+                onClick = { scope.launch { drawerState.close() } },
+                modifier = Modifier.padding(horizontal = 12.dp)
+            )
+
+            HorizontalDivider(modifier = Modifier.padding(horizontal = 28.dp, vertical = 16.dp))
+
+            val state by uiState.collectAsStateWithLifecycle()
+
+            if (state.isSignedIn) {
+                NavigationDrawerItem(
+                    label = { Text(stringResource(R.string.my_account)) },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountCircle, contentDescription = null
+                        )
+                    },
+                    selected = false,
+                    onClick = onMyAccountClick,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+
+                NavigationDrawerItem(
+                    label = { Text(text = stringResource(R.string.my_tickets)) },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_confirmation_number_24),
+                            contentDescription = null
+                        )
+                    },
+                    selected = false,
+                    onClick = onMyTicketsClick,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+
+                NavigationDrawerItem(
+                    label = { Text(text = stringResource(R.string.passengers)) },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_groups_24),
+                            contentDescription = null
+                        )
+                    },
+                    selected = false,
+                    onClick = onPassengersClick,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            } else {
+                NavigationDrawerItem(
+                    label = { Text(text = stringResource(R.string.login)) },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountCircle, contentDescription = null
+                        )
+                    },
+                    selected = false,
+                    onClick = onLoginClick,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            }
+        }
+    }) {
+        content()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeNavigationDrawerPreview() {
+    PKPAndroidTheme {
+        HomeNavigationDrawer(
+            uiState = MutableStateFlow(HomeUiState(isSignedIn = false)),
+            drawerState = rememberDrawerState(
+                initialValue = DrawerValue.Open
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignedInHomeNavigationDrawerPreview() {
+    PKPAndroidTheme {
+        HomeNavigationDrawer(
+            uiState = MutableStateFlow(HomeUiState(isSignedIn = true)),
+            drawerState = rememberDrawerState(
+                initialValue = DrawerValue.Open
+            )
+        )
+    }
+}
--- app/src/main/java/com/wentura/pkp_android/compose/home/HomeScreen.kt
+++ app/src/main/java/com/wentura/pkp_android/compose/home/HomeScreen.kt
@@ -6,23 +6,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
@@ -41,7 +34,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -95,93 +87,121 @@ fun HomeScreen(
     val snackbarHostState = remember { SnackbarHostState() }
     val context = LocalContext.current
 
-    ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
-        ModalDrawerSheet(modifier = Modifier.width(252.dp)) {
-            Text(
-                stringResource(R.string.app_name),
-                modifier = Modifier.padding(start = 28.dp, end = 28.dp, top = 16.dp)
-            )
-
-            HorizontalDivider(modifier = Modifier.padding(horizontal = 28.dp, vertical = 16.dp))
-
-            NavigationDrawerItem(
-                label = { Text(text = stringResource(R.string.connection_search)) },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.Search, contentDescription = null
-                    )
-                },
-                selected = true,
-                onClick = { scope.launch { drawerState.close() } },
-                modifier = Modifier.padding(horizontal = 12.dp)
-            )
-
-            HorizontalDivider(modifier = Modifier.padding(horizontal = 28.dp, vertical = 16.dp))
-
-            val state by uiState.collectAsStateWithLifecycle()
-
-            if (state.isSignedIn) {
-                NavigationDrawerItem(
-                    label = { Text(stringResource(R.string.my_account)) },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Outlined.AccountCircle, contentDescription = null
-                        )
-                    },
-                    selected = false,
-                    onClick = onMyAccountClick,
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                )
-
-                NavigationDrawerItem(
-                    label = { Text(text = stringResource(R.string.my_tickets)) },
-                    icon = {
-                        Icon(
-                            painter = painterResource(R.drawable.outline_confirmation_number_24),
-                            contentDescription = null
-                        )
-                    },
-                    selected = false,
-                    onClick = onMyTicketsClick,
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                )
-
-                NavigationDrawerItem(
-                    label = { Text(text = stringResource(R.string.passengers)) },
-                    icon = {
-                        Icon(
-                            painter = painterResource(R.drawable.outline_groups_24),
-                            contentDescription = null
-                        )
-                    },
-                    selected = false,
-                    onClick = onPassengersClick,
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                )
-            } else {
-                NavigationDrawerItem(
-                    label = { Text(text = stringResource(R.string.login)) },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Outlined.AccountCircle, contentDescription = null
-                        )
-                    },
-                    selected = false,
-                    onClick = onLoginClick,
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                )
-            }
-        }
-    }) {
+    HomeNavigationDrawer(
+        uiState = uiState,
+        drawerState = drawerState,
+        scope = scope,
+        onLoginClick = onLoginClick,
+        onMyAccountClick = onMyAccountClick,
+        onMyTicketsClick = onMyTicketsClick,
+        onPassengersClick = onPassengersClick
+    ) {
         Scaffold(topBar = { TopAppBar(scope, drawerState) },
             modifier = Modifier.fillMaxSize(),
             snackbarHost = {
                 SnackbarHost(hostState = snackbarHostState)
             }) { innerPadding ->
-            Fields(
+            var departureStationText by rememberSaveable { mutableStateOf("") }
+            var arrivalStationText by rememberSaveable { mutableStateOf("") }
+
+            val departureDate = rememberSaveable {
+                mutableStateOf(
+                    DateFormat.getDateInstance().format(Calendar.getInstance().time)
+                )
+            }
+
+            val departureTime = rememberSaveable {
+                mutableStateOf(
+                    DateFormat.getTimeInstance(DateFormat.SHORT).format(Calendar.getInstance().time)
+                )
+            }
+
+            val showDatePicker = rememberSaveable { mutableStateOf(false) }
+
+            if (showDatePicker.value) {
+                DatePicker(showDatePicker, departureDate)
+            }
+
+            val showTimePicker = rememberSaveable { mutableStateOf(false) }
+
+            if (showTimePicker.value) {
+                TimePicker(showTimePicker, departureTime)
+            }
+
+            Column(
                 modifier = Modifier.padding(innerPadding),
-                onSearchClick = onSearchClick,
-            )
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                    label = { Text(stringResource(R.string.departure_station)) },
+                    onValueChange = { departureStationText = it },
+                    value = departureStationText,
+                    singleLine = true,
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp)
+                        .padding(top = 20.dp, bottom = 10.dp)
+                        .fillMaxWidth()
+                )
+
+                OutlinedTextField(
+                    label = { Text(stringResource(R.string.arrival_station)) },
+                    onValueChange = { arrivalStationText = it },
+                    value = arrivalStationText,
+                    singleLine = true,
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp)
+                        .padding(vertical = 10.dp)
+                        .fillMaxWidth()
+                )
+
+                Row {
+                    OutlinedTextField(
+                        label = { Text(stringResource(R.string.departure_date)) },
+                        value = departureDate.value,
+                        onValueChange = {},
+                        readOnly = true,
+                        enabled = false,
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .padding(start = 20.dp, end = 10.dp)
+                            .weight(1f)
+                            .clickable {
+                                showDatePicker.value = true
+                            },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            disabledTextColor = MaterialTheme.colorScheme.onSurface,
+                            disabledBorderColor = MaterialTheme.colorScheme.outline,
+                            disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        )
+                    )
+
+                    OutlinedTextField(
+                        label = { Text(stringResource(R.string.departure_time)) },
+                        value = departureTime.value,
+                        onValueChange = {},
+                        readOnly = true,
+                        enabled = false,
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .padding(start = 10.dp, end = 20.dp)
+                            .weight(1f)
+                            .clickable {
+                                showTimePicker.value = true
+                            },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            disabledTextColor = MaterialTheme.colorScheme.onSurface,
+                            disabledBorderColor = MaterialTheme.colorScheme.outline,
+                            disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        )
+                    )
+                }
+
+                Button(
+                    onClick = onSearchClick, modifier = Modifier.padding(10.dp)
+                ) {
+                    Text(stringResource(R.string.search))
+                }
+            }
 
             val state by uiState.collectAsStateWithLifecycle()
 
@@ -209,131 +229,12 @@ private fun TopAppBar(
     })
 }
 
-@Composable
-fun Fields(
-    modifier: Modifier = Modifier,
-    onSearchClick: () -> Unit = {},
-    departureStation: String = "",
-    arrivalStation: String = "",
-) {
-    var departureStationText by rememberSaveable { mutableStateOf(departureStation) }
-    var arrivalStationText by rememberSaveable { mutableStateOf(arrivalStation) }
-    val departureDate = rememberSaveable {
-        mutableStateOf(
-            DateFormat.getDateInstance().format(Calendar.getInstance().time)
-        )
-    }
-
-    val departureTime = rememberSaveable {
-        mutableStateOf(
-            DateFormat.getTimeInstance(DateFormat.SHORT).format(Calendar.getInstance().time)
-        )
-    }
-
-    val showDatePicker = rememberSaveable { mutableStateOf(false) }
-
-    if (showDatePicker.value) {
-        DatePicker(showDatePicker, departureDate)
-    }
-
-    val showTimePicker = rememberSaveable { mutableStateOf(false) }
-
-    if (showTimePicker.value) {
-        TimePicker(showTimePicker, departureTime)
-    }
-
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        OutlinedTextField(
-            label = { Text(stringResource(R.string.departure_station)) },
-            onValueChange = { departureStationText = it },
-            value = departureStationText,
-            singleLine = true,
-            modifier = Modifier
-                .padding(horizontal = 20.dp)
-                .padding(top = 20.dp, bottom = 10.dp)
-                .fillMaxWidth()
-        )
-
-        OutlinedTextField(
-            label = { Text(stringResource(R.string.arrival_station)) },
-            onValueChange = { arrivalStationText = it },
-            value = arrivalStationText,
-            singleLine = true,
-            modifier = Modifier
-                .padding(horizontal = 20.dp)
-                .padding(vertical = 10.dp)
-                .fillMaxWidth()
-        )
-
-        Row {
-            OutlinedTextField(
-                label = { Text(stringResource(R.string.departure_date)) },
-                value = departureDate.value,
-                onValueChange = {},
-                readOnly = true,
-                enabled = false,
-                modifier = Modifier
-                    .padding(vertical = 10.dp)
-                    .padding(start = 20.dp, end = 10.dp)
-                    .weight(1f)
-                    .clickable {
-                        showDatePicker.value = true
-                    },
-                colors = OutlinedTextFieldDefaults.colors(
-                    disabledTextColor = MaterialTheme.colorScheme.onSurface,
-                    disabledBorderColor = MaterialTheme.colorScheme.outline,
-                    disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                )
-            )
-
-            OutlinedTextField(
-                label = { Text(stringResource(R.string.departure_time)) },
-                value = departureTime.value,
-                onValueChange = {},
-                readOnly = true,
-                enabled = false,
-                modifier = Modifier
-                    .padding(vertical = 10.dp)
-                    .padding(start = 10.dp, end = 20.dp)
-                    .weight(1f)
-                    .clickable {
-                        showTimePicker.value = true
-                    },
-                colors = OutlinedTextFieldDefaults.colors(
-                    disabledTextColor = MaterialTheme.colorScheme.onSurface,
-                    disabledBorderColor = MaterialTheme.colorScheme.outline,
-                    disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                )
-            )
-        }
-
-        Button(
-            onClick = onSearchClick, modifier = Modifier.padding(10.dp)
-        ) {
-            Text(stringResource(R.string.search))
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun HomePreview() {
     PKPAndroidTheme {
         HomeScreen(
             uiState = MutableStateFlow(HomeUiState(isSignedIn = false))
-        )
-    }
-}
-
-@Preview
-@Composable
-fun NavigationDrawerPreview() {
-    PKPAndroidTheme {
-        HomeScreen(
-            uiState = MutableStateFlow(HomeUiState(isSignedIn = false)),
-            drawerState = rememberDrawerState(
-                initialValue = DrawerValue.Open
-            )
         )
     }
 }
