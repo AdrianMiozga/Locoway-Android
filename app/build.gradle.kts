1d76d6c435bc3a168d1dd5dd6d4168368aabef55
Adrian Miozga <AdrianMiozga@outlook.com>
2024-06-03 20:35:59 +0200

    Bump version to `0.1.3`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.wentura.pkp_android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
 
         vectorDrawables { useSupportLibrary = true }
     }
