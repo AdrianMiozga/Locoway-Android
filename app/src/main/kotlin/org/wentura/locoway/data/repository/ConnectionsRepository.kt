663fbefb8a3fa68cba262f729672c47cf0f76a2e
Adrian Miozga <AdrianMiozga@outlook.com>
2025-04-26 09:48:21 +0200

    Fix connections screen

--- app/src/main/kotlin/org/wentura/locoway/data/repository/ConnectionsRepository.kt
+++ app/src/main/kotlin/org/wentura/locoway/data/repository/ConnectionsRepository.kt
@@ -38,38 +38,38 @@ class ConnectionsRepository @Inject constructor(private val koleoService: KoleoS
                 arrivalStation,
             )
 
-        connections.putAll(
-            koleoSearchResponse.connections
-                .map {
-                    val trainId = it.trainIds.first()
-                    val trainMap = koleoSearchResponse.trains.associateBy { train -> train.id }
-                    val koleoFormatter = DateTimeFormatter.ofPattern("HH:mm:ss yyyy-MM-dd")
-                    val price = koleoService.getPrices(it.priceId).price
+        val newConnections = koleoSearchResponse.connections
+            .map {
+                val trainId = it.trainIds.first()
+                val trainMap = koleoSearchResponse.trains.associateBy { train -> train.id }
+                val koleoFormatter = DateTimeFormatter.ofPattern("HH:mm:ss yyyy-MM-dd")
+                val price = koleoService.getPrices(it.priceId).price
 
-                    Connection(
-                        trainId,
-                        trainMap[trainId]?.trainNumber
-                            ?: throw IllegalStateException("Unknown train"),
-                        TrainBrand.valueOf(
-                            trainMap[trainId]?.trainBrand
-                                ?: throw IllegalStateException("Unknown train")
-                        ),
-                        price.ticketPrice,
-                        departureStation,
-                        arrivalStation,
-                        LocalDateTime.parse(it.departureDateTime, koleoFormatter),
-                        LocalDateTime.parse(it.arrivalDateTime, koleoFormatter),
-                        price.dogPrice ?: "0",
-                        price.bikePrice ?: "0",
-                        price.luggagePrice ?: "0",
-                    )
-                }
-                .associateBy { it.trainId }
-        )
+                Connection(
+                    trainId,
+                    trainMap[trainId]?.trainNumber
+                        ?: throw IllegalStateException("Unknown train"),
+                    TrainBrand.valueOf(
+                        trainMap[trainId]?.trainBrand
+                            ?: throw IllegalStateException("Unknown train")
+                    ),
+                    price.ticketPrice,
+                    departureStation,
+                    arrivalStation,
+                    LocalDateTime.parse(it.departureDateTime, koleoFormatter),
+                    LocalDateTime.parse(it.arrivalDateTime, koleoFormatter),
+                    price.dogPrice ?: "0",
+                    price.bikePrice ?: "0",
+                    price.luggagePrice ?: "0",
+                )
+            }
+            .associateBy { it.trainId }
+
+        connections.putAll(newConnections)
 
         Log.d(TAG, "Added new connections")
 
-        return connections
+        return newConnections
     }
 
     fun getConnectionByIdFromCache(id: Long): Connection {
